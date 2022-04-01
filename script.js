2dd891a0ca222958c30f3e9f90a0aab3c1a8fd15
Igor Sampaio <igorsamf@gmail.com>
2022-04-01 19:42:32 -0300

    requisito 18

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//////////////////////////////////////// VALIDAÇÃO DE LOGIN
 document.getElementById('loginButton').addEventListener('click', clickLogin);
 
 function clickLogin() {
@@ -11,3 +12,29 @@ function clickLogin() {
 alert('Email ou senha inválidos.');
 }
 
+//////////////////////////////////////// VALIDAÇÃO PARA ENVIAR O FORMULÁRIO
+
+const checkboxId = document.getElementById('agreement');
+const buttonId = document.getElementById('submit-btn');
+checkboxId.addEventListener('change', sendForm);
+
+
+function sendForm() {
+    if(this.checked) {
+        buttonId.disabled = false;
+        return;
+    }
+    buttonId.disabled = true;
+}
+
+
+
+
+// SE A CHECKBOX ESTIVER SELECIONADA
+//     >>> BOTÃO ATIVO
+
+// SE A CHECKBOX NÃO ESTIVER SELECIONADA
+//     >>> BOTÃO INATIVO
+
+//     o id do checkbox é 'agreement'
+//     o id do botão é 'submit-btn'
