e516181d0b1471d16bb0c1a9f385a4870da88c6f
Diego M. Cezar <dmcezar@gmail.com>
2022-04-02 02:13:01 -0300

    Adicionados comentários para clareza de leitura do requisito 21.

--- script.js
+++ script.js
@@ -1,70 +1,74 @@
 //////////////////////////////////////// VALIDAÇÃO DE LOGIN
-document.getElementById('loginButton').addEventListener('click', clickLogin);
+document.getElementById("loginButton").addEventListener("click", clickLogin);
 
 function clickLogin() {
-    let loginEmail = document.getElementById('loginEmail');
-    let loginSenha = document.getElementById('loginSenha');
-    
-    if(loginEmail.value === 'tryber@teste.com' && loginSenha.value === '123456') {
-    alert('Olá, Tryber!');
+  let loginEmail = document.getElementById("loginEmail");
+  let loginSenha = document.getElementById("loginSenha");
+
+  if (
+    loginEmail.value === "tryber@teste.com" &&
+    loginSenha.value === "123456"
+  ) {
+    alert("Olá, Tryber!");
     return;
-    }
-alert('Email ou senha inválidos.');
+  }
+  alert("Email ou senha inválidos.");
 }
 
 //////////////////////////////////////// VALIDAÇÃO PARA ENVIAR O FORMULÁRIO
 
-const checkboxId = document.getElementById('agreement');
-const buttonId = document.getElementById('submit-btn');
-checkboxId.addEventListener('change', sendForm);
-
+const checkboxId = document.getElementById("agreement");
+const buttonId = document.getElementById("submit-btn");
+checkboxId.addEventListener("change", sendForm);
 
 function sendForm() {
-    if(this.checked) {
-        buttonId.disabled = false;
-        return;
-    }
-    buttonId.disabled = true;
+  if (this.checked) {
+    buttonId.disabled = false;
+    return;
+  }
+  buttonId.disabled = true;
 }
 
 //////////////////////////////////////// LÓGICA DO CONTADOR DE CARACTERES
 
-document.getElementById('textarea').addEventListener('input', atualizaContador);
+document.getElementById("textarea").addEventListener("input", atualizaContador);
 
-function atualizaContador () {
-    document.getElementById('counter').innerText = 500 - this.value.length;
+function atualizaContador() {
+  document.getElementById("counter").innerText = 500 - this.value.length;
 }
 
 //////////////////////////////////////// BOTÃO DE ENVIAR
 
 // Armazena o formulário em uma variável
-const formulario = document.getElementById('evaluation-form');
+const formulario = document.getElementById("evaluation-form");
 
 // Adiciona o escutador ao botão que irá disparar a função requisito21
-buttonId.addEventListener('click', requisito21);
+buttonId.addEventListener("click", requisito21);
 
-function requisito21 (){
-    let dadosCadastrais = {
-        nome: `Nome: ${document.getElementById('input-name').value} ${document.getElementById('input-lastname').value}`,
-        email: `Email: ${document.getElementById('input-email').value}`,
-        casa: `Casa: ${document.getElementById('house').value}`,
-        familia: `Família: ????`,
-        materias: `Matérias: ????`,
-        avaliacao: `Avaliação: ????`,
-        obs: `Observações: ${document.getElementById('textarea').value}`
-    }
+function requisito21() {
+  // Objeto criado para armazenar os dados do formulário a serem exibidos no final.
+  let dadosCadastrais = {
+    nome: `Nome: ${document.getElementById("input-name").value} ${
+      document.getElementById("input-lastname").value
+    }`,
+    email: `Email: ${document.getElementById("input-email").value}`,
+    casa: `Casa: ${document.getElementById("house").value}`,
+    familia: `Família: ????`,
+    materias: `Matérias: ????`,
+    avaliacao: `Avaliação: ????`,
+    obs: `Observações: ${document.getElementById("textarea").value}`,
+  };
 
-    console.log(dadosCadastrais[0])
+  // Apaga todo o conteúdo do formulário.
+  formulario.innerHTML = "";
 
-    formulario.innerHTML = "";
-
-    for (let i = 0; i < 7; i += 1) {
-        const novoCampo = document.createElement('p');
-        novoCampo.innerText = Object.values(dadosCadastrais)[i];
-        formulario.appendChild(novoCampo);
-    }
+  // O loop abaixo cria elementos <p>, insere neles o texto e os coloca como filhos do formulário que está vazio.
+  for (let i = 0; i < Object.keys(dadosCadastrais).length; i += 1) {
+    const novoCampo = document.createElement("p");
+    novoCampo.innerText = Object.values(dadosCadastrais)[i];
+    formulario.appendChild(novoCampo);
+  }
 }
-
 
 // let dadosCadastrais = {
 //     nome: 1,
