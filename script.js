a04cfe00080bf7ef60754fe07e06fc42ea8f5f83
Diego M. Cezar <dmcezar@gmail.com>
2022-04-02 02:07:35 -0300

    Requisito 21 em progresso. Com dificuldade em pegar os valores praa familia, materias e avaliaçao no objeto criado.

--- script.js
+++ script.js
@@ -36,3 +36,44 @@ function atualizaContador () {
 }
 
 //////////////////////////////////////// BOTÃO DE ENVIAR
+
+// Armazena o formulário em uma variável
+const formulario = document.getElementById('evaluation-form');
+
+// Adiciona o escutador ao botão que irá disparar a função requisito21
+buttonId.addEventListener('click', requisito21);
+
+function requisito21 (){
+    let dadosCadastrais = {
+        nome: `Nome: ${document.getElementById('input-name').value} ${document.getElementById('input-lastname').value}`,
+        email: `Email: ${document.getElementById('input-email').value}`,
+        casa: `Casa: ${document.getElementById('house').value}`,
+        familia: `Família: ????`,
+        materias: `Matérias: ????`,
+        avaliacao: `Avaliação: ????`,
+        obs: `Observações: ${document.getElementById('textarea').value}`
+    }
+
+    console.log(dadosCadastrais[0])
+
+    formulario.innerHTML = "";
+
+    for (let i = 0; i < 7; i += 1) {
+        const novoCampo = document.createElement('p');
+        novoCampo.innerText = Object.values(dadosCadastrais)[i];
+        formulario.appendChild(novoCampo);
+    }
+}
+
+
+// let dadosCadastrais = {
+//     nome: 1,
+//     email: 1,
+//     casa: 1,
+//     familia: `Família: ????`,
+//     materias: `Matérias: ????`,
+//     avaliacao: `Avaliação: ????`,
+//     obs: 1
+// }
+
+// console.log(Object.keys(dadosCadastrais)[1])
