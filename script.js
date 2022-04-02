d2793dcc871d8285e60357d2bde185a911d0c9eb
Diego M. Cezar <dmcezar@gmail.com>
2022-04-02 09:48:50 -0300

    Limpeza básica de código para o lint.

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-//////////////////////////////////////// VALIDAÇÃO DE LOGIN
-document.getElementById("loginButton").addEventListener("click", clickLogin);
+// VALIDAÇÃO DE LOGIN
 
 function clickLogin() {
-  let loginEmail = document.getElementById("loginEmail");
-  let loginSenha = document.getElementById("loginSenha");
+  const loginEmail = document.getElementById('loginEmail');
+  const loginSenha = document.getElementById('loginSenha');
 
   if (
-    loginEmail.value === "tryber@teste.com" &&
-    loginSenha.value === "123456"
+    loginEmail.value === 'tryber@teste.com' &&
+    loginSenha.value === '123456'
   ) {
-    alert("Olá, Tryber!");
+    alert('Olá, Tryber!');
     return;
   }
-  alert("Email ou senha inválidos.");
+  alert('Email ou senha inválidos.');
 }
 
-//////////////////////////////////////// VALIDAÇÃO PARA ENVIAR O FORMULÁRIO
+document.getElementById('loginButton').addEventListener('click', clickLogin);
 
-const checkboxId = document.getElementById("agreement");
-const buttonId = document.getElementById("submit-btn");
-checkboxId.addEventListener("change", sendForm);
+// VALIDAÇÃO PARA ENVIAR O FORMULÁRIO
+
+const checkboxId = document.getElementById('agreement');
+const buttonId = document.getElementById('submit-btn');
 
 function sendForm() {
   if (this.checked) {
@@ -29,55 +29,45 @@ function sendForm() {
   buttonId.disabled = true;
 }
 
-//////////////////////////////////////// LÓGICA DO CONTADOR DE CARACTERES
+checkboxId.addEventListener('change', sendForm);
 
-document.getElementById("textarea").addEventListener("input", atualizaContador);
+// LÓGICA DO CONTADOR DE CARACTERES
 
 function atualizaContador() {
-  document.getElementById("counter").innerText = 500 - this.value.length;
+  document.getElementById('counter').innerText = 500 - this.value.length;
 }
 
-//////////////////////////////////////// BOTÃO DE ENVIAR
+document.getElementById('textarea').addEventListener('input', atualizaContador);
+
+// BOTÃO DE ENVIAR
 
 // Armazena o formulário em uma variável
-const formulario = document.getElementById("evaluation-form");
-
-// Adiciona o escutador ao botão que irá disparar a função requisito21
-buttonId.addEventListener("click", requisito21);
+const formulario = document.getElementById('evaluation-form');
 
 function requisito21() {
   // Objeto criado para armazenar os dados do formulário a serem exibidos no final.
   let dadosCadastrais = {
-    nome: `Nome: ${document.getElementById("input-name").value} ${
-      document.getElementById("input-lastname").value
+    nome: `Nome: ${document.getElementById('input-name').value} ${
+      document.getElementById('input-lastname').value
     }`,
-    email: `Email: ${document.getElementById("input-email").value}`,
-    casa: `Casa: ${document.getElementById("house").value}`,
+    email: `Email: ${document.getElementById('input-email').value}`,
+    casa: `Casa: ${document.getElementById('house').value}`,
     familia: `Família: ????`,
     materias: `Matérias: ????`,
     avaliacao: `Avaliação: ????`,
-    obs: `Observações: ${document.getElementById("textarea").value}`,
+    obs: `Observações: ${document.getElementById('textarea').value}`,
   };
 
   // Apaga todo o conteúdo do formulário.
-  formulario.innerHTML = "";
+  formulario.innerHTML = '';
 
   // O loop abaixo cria elementos <p>, insere neles o texto e os coloca como filhos do formulário que está vazio.
   for (let i = 0; i < Object.keys(dadosCadastrais).length; i += 1) {
-    const novoCampo = document.createElement("p");
+    const novoCampo = document.createElement('p');
     novoCampo.innerText = Object.values(dadosCadastrais)[i];
     formulario.appendChild(novoCampo);
   }
 }
 
-// let dadosCadastrais = {
-//     nome: 1,
-//     email: 1,
-//     casa: 1,
-//     familia: `Família: ????`,
-//     materias: `Matérias: ????`,
-//     avaliacao: `Avaliação: ????`,
-//     obs: 1
-// }
-
-// console.log(Object.keys(dadosCadastrais)[1])
+// Adiciona o escutador ao botão que irá disparar a função requisito21
+buttonId.addEventListener('click', requisito21);
